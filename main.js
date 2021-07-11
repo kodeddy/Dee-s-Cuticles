a944534badfe185b7a11025a3122907d5b49a12f
kodeddy <edidiongdan1@gmail.com>
2021-07-10 23:07:25 -0500

    starting to use .gsap

---
+++ main.js
@@ -0,0 +1,5 @@
+gsap.from(".balzydp", {duration: 3, y: -900, backgroundColor: "#560563", ease: "bounce" });
+
+gsap.to(".balzyfirsttext", {duration: 2, y: 200, x:100, ease: "bounce"});
+gsap.to(".balzyfirsttext", {duration: 3, x: 100, ease: "bounce"});
+ 
