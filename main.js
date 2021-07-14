8377bedbf2366fddd178ce3b0be51573876cc682
kodeddy <edidiongdan1@gmail.com>
2021-07-14 14:31:49 -0500

    working on the flip cards

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-gsap.from(".balzydp", {duration: 3, y: -900, backgroundColor: "#560563", ease: "bounce" });
+gsap.from(".balzydp", {duration: 3, y: -900, backgroundColor: "#560563", ease: "bounce"});
 
 gsap.to(".balzyfirsttext", {duration: 2, y: 200, x:100, ease: "bounce"});
 gsap.to(".balzyfirsttext", {duration: 3, x: 100, ease: "bounce"});
  
+gsap.to(".flip-card-front h1", {duration: 3, y: 300, repeat: -1, yoyo: true, ease: "power0", stagger: 1});
+
